0c2f37072c71fd6319589911acb120691cad7ae4
Maya Thompson <mayaandromedathompson@gmail.com>
2025-04-15 10:09:06 -0500

    Workbook 2 Module 1 and 2 initial commit

---
+++ FullNameGenerator/src/main/java/com/pluralsight/FullNameApplication.java
@@ -0,0 +1,39 @@
+package com.pluralsight;
+
+import java.util.*;
+
+public class FullNameApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("What is your name?");
+
+        String firstName = nameGet("First name:");
+        String middleName = nameGet("Middle name:");
+        String lastName = nameGet("Last name:");
+        String suffix = nameGet("Suffix:");
+
+        String fullName;
+        if (middleName.isEmpty() && suffix.isEmpty()) {
+            fullName = firstName + " " + lastName;
+        }
+        else if (middleName.isEmpty()) {
+            fullName = firstName + " " + lastName + ", " + suffix;
+        }
+        else if (suffix.isEmpty()) {
+            fullName = firstName + " " + middleName + " " + lastName;
+        }
+        else {
+            fullName = firstName + " " + middleName + " " + lastName + ", " + suffix;
+        }
+
+        System.out.println("Fullname: " + fullName);
+    }
+
+    static String nameGet(String message) {
+        System.out.print(message);
+        Scanner scanner = new Scanner(System.in);
+        String response = scanner.nextLine();
+        return response.trim();
+    }
+}
---
+++ FullNameParser/src/main/java/com/pluralsight/FullNameParser.java
@@ -0,0 +1,27 @@
+package com.pluralsight;
+
+import java.util.*;
+
+public class FullNameParser {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("what is your full name?\nEnter here: ");
+        String fullName = scanner.nextLine();
+
+        String[] parsedName = fullName.split(" ");
+        String firstName = parsedName[0];
+        String lastName = parsedName[parsedName.length - 1];
+        String middleName = parsedName[1];
+
+        if (middleName == lastName) {
+            System.out.println("First name: " + firstName + "\nMiddle name: (none)\nLast name: " + lastName);
+        }
+        else {
+            System.out.println("First name: " + firstName + "\nMiddle name: " + middleName + "\nLast name: " + lastName);
+        }
+
+    }
+
+}
