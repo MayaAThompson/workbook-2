c5f118e234c35e35a2a86c44ba8533a6ca8e9e34
Maya Thompson <mayaandromedathompson@gmail.com>
2025-04-15 16:15:49 -0500

    trimmed and reformatted name, firstName, lastName

--- TheaterReservations/src/main/java/com/pluralsight/TheaterReservations.java
+++ TheaterReservations/src/main/java/com/pluralsight/TheaterReservations.java
@@ -8,7 +8,10 @@ public class TheaterReservations {
 
     public static void main(String[] args) {
 
-        String name = messageAndResponse("Please enter your name: ");
+        String name = messageAndResponse("Please enter your name: ").trim();
+        String[] firstLast = name.split(" ");
+        String firstName = firstLast[0];
+        String lastName = firstLast[1];
 
         String dateInput = messageAndResponse("What date will you be coming? (MM/dd/yyyy): ");
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
@@ -16,7 +19,7 @@ public class TheaterReservations {
 
         int tickets = Integer.parseInt(messageAndResponse("How many tickets would you like?: "));
 
-        System.out.println(tickets + " tickets reserved for " + date + " for " + name);
+        System.out.println(tickets + " tickets reserved for " + date + " for " + lastName + ", " + firstName);
     }
 
     static String messageAndResponse(String message) {
