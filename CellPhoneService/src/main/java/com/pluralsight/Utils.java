c8830d747dcf83b2a4856ec6d5d0bb95abf42f8a
Maya Thompson <mayaandromedathompson@gmail.com>
2025-04-17 16:08:31 -0500

    Added functionality comments to Utils.java

--- CellPhoneService/src/main/java/com/pluralsight/Utils.java
+++ CellPhoneService/src/main/java/com/pluralsight/Utils.java
@@ -2,16 +2,19 @@ package com.pluralsight;
 
 import java.util.Scanner;
 
+//common utilities
 public class Utils {
 
     static Scanner scanner = new Scanner(System.in);
 
-    static String messageAndResponse(String message) {
+    //passes in a String message, prints that message to the console and returns user input on same line
+    public static String messageAndResponse(String message) {
         System.out.print(message);
         return scanner.nextLine();
     }
 
-    static void pauseReturn() {
+    //a way to add a pause also adds one line of whitespace in console. continues on "Enter" key press
+    public static void pauseReturn() {
         scanner.nextLine();
     }
 }
